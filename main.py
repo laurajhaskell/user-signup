8b29f8144317461b221d83b4170caf2d94900ea1
Laura Haskell <laurajhaskell@yahoo.com>
2018-10-21 14:53:08 -0500

    v2

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from flask import Flask, request, redirect, render_template
 
 app = Flask(__name__)
-
 app.config['DEBUG'] = True
 
 
@@ -23,11 +22,14 @@ def validate():
     email_error = ''
 
     #error1 - The user leaves any of the following fields empty: username, password, verify password.
-    if len(username) < 3 or len(username) > 20:
+    if username == '':
         username_error = "That's not a valid username."
 
-    if len(password) < 3 or len(password) > 20:
+    if password == '':
         password_error = "That's not a valid password."
+
+    if verify == '':
+        verify_password_error = "Passwords do not match."
 
     #error2 - The user's username or password is not valid -- for example, it contains a space character
     if ' ' in username:
@@ -36,11 +38,19 @@ def validate():
     if ' ' in password:
         password_error = "That's not a valid password."
 
+    if len(username) < 3 or len(username) > 20:
+        username_error = "That's not a valid username."
+
+    if len(password) < 3 or len(password) > 20:
+        password_error = "That's not a valid password."
+
     #error3 - The user's password and password-confirmation do not match.
-    if verify == '':
+    if password != verify:
         verify_password_error = "Passwords do not match."
 
-    #error4 - The user provides an email, but it's not a valid email. Note: the email field may be left empty, but if there is content in it, then it must be validated. The criteria for a valid email address in this assignment are that it has a single @, a single ., contains no spaces, and is between 3 and 20 characters long.
+    #error4 - The user provides an email, but it's not a valid email. 
+    # Note: the email field may be left empty, but if there is content in it, then it must be validated. 
+    # The criteria for a valid email address in this assignment are that it has a single @, a single ., contains no spaces, and is between 3 and 20 characters long.
     if email:
         if len(email) < 3 or len(email) > 20:
             email_error = "That's not a valid email."
@@ -67,6 +77,6 @@ def validate():
 @app.route("/welcome")
 def welcome():
     username = request.args.get('username')
-    return render_template('welcome.html')
+    return render_template('welcome.html', username=username)
 
 app.run()
